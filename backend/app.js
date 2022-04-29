0a7ec3aaacb4a4bbdaee37102ca5dddf7f7d48a4
Subramaniam Jeyaseelan <jeyaseelan421@gmail.com>
2022-04-29 23:06:21 +0530

    mongo db, routes initial

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require("../routes/book-routes");
+
+
+const app = express();
+
+// Not needed
+// //middleware
+// app.use('/',(req,res,next)=>{
+//     res.send("This is our starting app");
+// })
+
+
+mongoose.connect("mongodb+srv://admin:UbP1dH9ucSO3SGf7@cluster0.da8fs.mongodb.net/Book-Store?retryWrites=true&w=majority")
+.then(()=>console.log('Connected to Database'))
+.then(()=>{
+    app.listen(5000)
+}).catch((err)=>console.log(err));
+
+
+
+
+
+
+
+
---
+++ backend/routes/book-routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const Book = require("../model/Book");
+
+router.get("/",(req,res,next)=>{
+   let books;
+   try{
+       books = await Book.find();
+   } catch(err){
+       console.log(err);
+   }
+
+   if(!books){
+        return res.status(404).json({message:"No Books Found"})
+   }
+    return res.status(200).json({books});
+});
+
+module.exports = router;
+
+
